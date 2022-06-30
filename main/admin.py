bd67607ad8379678382127278e2295ec31cca8e5
xurizayniev <xurizayniev@gmail.com>
2022-06-30 21:15:30 +0500

    added templates files and message model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(MessageModel)
+class MessageModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'phone', 'email']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'email']
+    list_filter = ['created_at']
